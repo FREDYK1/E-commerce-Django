d06c833dc1fc412acdc65c66d2e285fbc09a924d
FREDYK1 <fkwekukankam@gmail.com>
2024-12-14 10:06:03 +0000

    Migrate database and register on admin page

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Customer model
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    password = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+# Product model
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
+# Order model
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    address = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=15, default='', blank=True, null=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product

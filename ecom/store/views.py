57e768b3ac75d2c50990df8d66da55033af47d0a
FREDYK1 <fkwekukankam@gmail.com>
2024-12-19 23:44:55 +0000

    Add an About page to the website

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -6,4 +6,4 @@ def home(request):
     return render(request, 'home.html', {"products": products})
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {})

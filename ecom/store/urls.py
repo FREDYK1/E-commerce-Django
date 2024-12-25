8435d31973dad7f3bfc7a33a04718fa0b10a0796
FREDYK1 <fkwekukankam@gmail.com>
2024-12-25 23:06:57 +0000

    Fixing bugs

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('login_user/', views.login_user, name='login'),
+    path('logout_user/', views.logout_user, name='logout'),
 ]

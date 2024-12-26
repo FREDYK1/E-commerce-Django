c2a54618d8f25acecef0c290558872e1ee49361e
FREDYK1 <fkwekukankam@gmail.com>
2024-12-26 01:02:55 +0000

    Implement the register feature

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('login_user/', views.login_user, name='login'),
     path('logout_user/', views.logout_user, name='logout'),
+    path('register_user/', views.register_user, name='register'),
 ]
